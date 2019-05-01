af065186a6c7dc03f17fe16445692ef23ed454ec
shuric <shuric80@gmail.com>
2019-05-01 22:26:54 +0300

    [upd] log level

--- log.py
+++ log.py
@@ -4,19 +4,21 @@ import os.path as ph
 import logging
 from logging.handlers import RotatingFileHandler
 import telebot
+import config
 
 
 logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
+LEVEL = logging.DEBUG if config.DEBUG else logging.INFO
+telebot.logger.setLevel(LEVEL)
 
 file_log = ph.join('log','ozzi_bot.log')
 handler = RotatingFileHandler(file_log, maxBytes = 100000, backupCount =5)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(LEVEL)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 
 ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
+ch.setLevel(LEVEL)
 ch.setFormatter(formatter)
 
 #logger.addHandler(ch)
