0b4cc1a9fffc1b2918d2a794a7d64db3a2746481
shuric <shuric80@gmail.com>
2019-05-12 13:49:03 +0300

    [upd] name group

--- custom_keybords.py
+++ custom_keybords.py
@@ -32,8 +32,9 @@ def keyboard_list_groups(q, id):
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     l_btns = list()
     for btn in q:
+        name = getattr(btn, 'name', 'Noname')
         callable_button = types.InlineKeyboardButton(
-            text = btn.name, callback_data= json.dumps(dict(id = id, button=btn.id)))
+            text = name[:30], callback_data= json.dumps(dict(id = id, button=btn.id)))
         l_btns.append(callable_button)
 
     keyboard.add(*l_btns)
